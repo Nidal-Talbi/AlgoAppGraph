763617eb29299ea7228aabeb8075481035c46866
Nidal Talbi <nidal.talbi7@gmail.com>
2022-03-14 14:33:33 +0100

    maj2

--- AlgoAppGraph/src/App.java
+++ AlgoAppGraph/src/App.java
@@ -1,5 +1,19 @@
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String args[]) {
+		System.setProperty("org.graphstream.ui", "swing");
+		
+		Graph graph = new SingleGraph("Tutorial 1");
+
+		graph.addNode("A");
+		graph.addNode("B");
+		graph.addNode("C");
+		graph.addEdge("AB", "A", "B");
+		graph.addEdge("BC", "B", "C");
+		graph.addEdge("CA", "C", "A");
+
+		graph.display();
+	}
 }
