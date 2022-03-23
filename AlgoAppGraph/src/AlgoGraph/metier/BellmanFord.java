400736569e9c5e8aff06ebf30393d12b57dcaa0d
Nidal-Talbi <nidal.talbi7@gmail.com>
2022-03-24 00:12:00 +0100

    X

---
+++ AlgoAppGraph/src/AlgoGraph/metier/BellmanFord.java
@@ -0,0 +1,66 @@
+package AlgoGraph.metier;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * BellmanFord
+ */
+public class BellmanFord {
+    private App app;
+    private HashMap<String,Integer> hashArc;
+    private LecteurFichier lfArc;
+    public BellmanFord()
+    {
+        this.hashArc = new HashMap<String,Integer>();
+        this.lfArc   = new LecteurFichier("arc.txt");
+
+        for (int i = 0; i < app.nbSommet(); i++) {
+            if (i == 0)
+            {
+                this.hashArc.put(app.getSommet(i), 0);
+            } else {
+                this.hashArc.put(app.getSommet(i), Integer.MAX_VALUE);
+            }
+        }
+        int cpt=0;
+        while (cpt < app.nbSommet() -1 ) {
+            for (String str : lfArc.getArc()) {
+                String res = lfArc.getArc().get(cpt); //(a,b)
+                String u   = String.valueOf(res.substring(1));
+                String v   = String.valueOf(res.substring(3));
+                if ( hashArc.get(v) > hashArc.get(u) + app.distance(u,v))
+                {
+                    hashArc.remove(v);
+                    hashArc.put(v, hashArc.get(u) + app.distance(u,v));
+                }
+            }
+        }
+    }
+
+    public Integer[] intoTab()
+    {
+        Integer[] tab = new Integer[this.hashArc.size()];
+        Integer cpt=0;
+        for (Map.Entry<String,Integer> entry : hashArc.entrySet()) {
+            tab[cpt] = entry.getValue();
+            cpt++;
+        }
+        Arrays.sort(tab);
+        return tab;
+    }
+
+    public String cheminPlusCourt()
+    {
+        Integer[] tab = this.intoTab();
+        String sRet = "";
+        for (int i = 0; i < this.hashArc.size(); i++) {
+            if (tab[i] == this.hashArc.get(this.app.getSommet(i)))
+            {
+                sRet += this.app.getSommet(i);
+            }
+        }
+        return sRet;
+    }
+}
